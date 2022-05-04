d3b5cf1a2c784b57938fba9afb98de85b37e1665
Subir Mondal <sprivacc0@gmail.com>
2022-05-04 13:07:27 +0530

    First try

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/header.css';
+
+export const Header = () => {
+  return (
+    <>
+        <div className='header'>
+            Header
+        </div>
+    </>
+  )
+}
